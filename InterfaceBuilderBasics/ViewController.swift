7e456b2e3eee8b928eafaefca173ac9d1a3c7ba5
codegangsk <kjhj0604@gmail.com>
2020-08-19 16:17:04 +0900

    first commit

--- InterfaceBuilderBasics/ViewController.swift
+++ InterfaceBuilderBasics/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeTitle(_ sender: Any) {
+        mainLabel.text = "This app rocks!"
+    }
+    
 }
 
